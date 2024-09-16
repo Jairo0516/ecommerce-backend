6cc4729408539c39dbaa4779ece4743f878b219a
Jairo0516 <jairoandres689@gmail.com>
2024-09-15 22:07:36 -0500

    Structure project

---
+++ src/main/java/com/ecommerce/controller/dto/response/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.ecommerce.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Data
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class ResponseDTO {
+
+    private StatusDTO status;
+
+}
